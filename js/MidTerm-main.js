fbd0ca3fc869cec9c420b532592e888c2148a652
wenhaowuuu <wenhaowu92@gmail.com>
2017-03-08 16:25:44 -0500

    MidTerm_EconomicIndicatorsChineseCities

--- js/MidTerm-main.js
+++ js/MidTerm-main.js
@@ -6,13 +6,20 @@
   Call getAndParseData to grab our dataset through a jQuery.ajax call ($.ajax)
 ===================== */
 //prepare the transformed data from csv
-var dataset = "https://github.com/wenhaowuuu/week-6/blob/9e23f73c999515c2066197c089430a0e34980bbc/EconomicIndicator_Chinesecities.js";
+
+//claim the global variables:
+
+var layerMappedMarkers;
+var slideNumber = 0;
+var parsedData;
+var dataset = "https://raw.githubusercontent.com/wenhaowuuu/MidTermFinal/master/data/EconomicIndicator_Chinesecities.geojson";
 
 //set up map
 var map = L.map('map', {
   center: [32.9670, 117.5370],
   zoom: 5
 });
+
 var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
@@ -21,155 +28,206 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   ext: 'png'
 }).addTo(map);
 
-//for indicators filter
-var highGDPFilter = _.filter(parsedData,function(item){
-  return item.properties.GDPlevel === "high";
-});
+//define the clickfunctions:
+var clickNextButton = function(){
+  var filteredData;
+  var html;
+  slideNumber++;
+  if(slideNumber == 1){
 
-var mediumGDPFilter =_.filter(parsedData,function(item){
-  return item.properties.GDPlevel === "medium";
-});
+    html = "<div></div>"
+         + '<h3>Map: Cities with High Level GDP</h3>'
+         + '<h4>what level is the GDP, please click!</h4>'
+         + '<p class="main">The GDP for this city is<span class="GDP"></span>.</p>'
 
-var lowGDPFilter =_.filter(parsedData,function(item){
-  return item.properties.GDPlevel === "low";
-});
+    $('.explanation').empty();
+    $('.explanation').append(html);
 
-//download data and parse it
-$(document).ready(function(dataset) {
-  $.ajax(dataset).done(function(data) {
-    console.log(dataset);
-    var parsedData = JSON.parse(data);
-    featureGroup = L.geoJson(parsedData, {
-      style: myStyle,
-      filter: myFilter
-    }).addTo(map);
-    console.log(parsedData);
-    featureGroup.eachLayer(eachFeatureFunction);
-  });
-});
+    $('#previous').show();
 
-var myStyle=function(feature){
-  switch(feature.properties.GDPlevel){
-    case "high":return{color:"#00FFFF"};
-    case "medium":return{color:"#0000FF"};
-    case "low":return{color:"#FF7F50"};
-  }
-  return {};
-};
-
-var myFilter = function(feature) {
-  if (feature.properties.GDPlevel===' ') {
-  return false;
-  }
-  else {
-    return true;
-  }
-};
-
-console.log('Included:', filtered_data1.length);
-console.log('Excluded:', filtered_out1.length);
-
-//slide functions
-var slide1 = function(){
-};
-
-
-var state = {
-  "slideNumber": 0, // slideNumber keeps track of what slide you are on. It should increase when you
-                    // click the next button and decrease when you click the previous button. It
-                    // should never get so large that it is bigger than the dataset. It should never
-                    // get so small that it is smaller than 0.
-  "mapped":undefined,
-  "slideData": [
-    {
-      "GDP level": "High",
-      "description": "high GDP cities",
-    },
-    {
-      "GDP level": "mediumn",
-      "description": "medium GDP cities",
-    },
-    {
-      "GDP level": "low",
-      "description": "low GDP cities",
-    }
-  ]
-};
-
-console.log(ChineseCities);
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-
-//clicking functions
-var clickNextButton = function() {
-  if (state.slideNumber<slideData.length){
-    state.slideNumber +=1;
-    console.log('slide'+state.slideNumber);
-  }
-  else state.slideNumber = 0;
-  return state.slideNumber;
-};
-
-var clickPreviousButton = function() {
-  if (state.slideNumber>0){
-    state.slideNumber -=1;
-    console.log('slide'+state.slideNumber);
-  }
-  else state.slideNumber = 0;
-  return state.slideNumber;
-};
-
-$('#next').click(function(){
-  clickNextButton();
-  document.getElementById("results").style.display = "inline";
-  if (state.slideNumber === 1){
-    state.mapped = L.geoJSON(highGDPFilter).addTo(map);
-    $('#main-heading').text('the high GDPlevel');
-    $('#slidenumber').text('Slide' + state.slideNumber);
-    $('#description').text(state.slideData[slideNumber-1].description);
-  }
-});
-
-$('#previous').click(function(){
-  clickPreviousButton();
-  document.getElementById("results").style.display = "inline";
-  if (state.slideNumber === slideData.length){
-    state.mapped = L.geoJSON(lowGDPFilter).addTo(map);
-    $('#main-heading').text('the low GDPlevel');
-    $('#slidenumber').text('Slide' + state.slideNumber);
-    $('#description').text(state.slideData[slideNumber-1].description);
-  }
-});
-
-
-
-$(document).ready(function(dataset){
-  $.ajax(dataset).done(function(data) {
-    var parsedData = JSON.parse(data);
-    featureGroup = L.geoJson(parsedData, {
-        style: MondayStyle,
-        filter: myFilter
-      }).addTo(map);
+    map.removeLayer(layerMappedMarkers);
+    filteredData = _.filter(parsedData.features,function(datum){
+      return datum.properties.GDPlevel === "high";
     });
+    filteredData = {
+      type: "FeatureCollection",
+      features: filteredData
+    };
+    layerMappedMarkers = L.geoJSON(filteredData)
+                          .eachLayer(function(layer){
+                            layer.on('click', function(){
+                              console.log('clicked');
+                              console.log(layer.feature.properties.GDP);
+                              $('.GDP').text(" " + layer.feature.properties.GDP);
+                            });
+                          })
+                          .addTo(map);
+
+
+  }else if(slideNumber == 2){
+    console.log(slideNumber);
+
+    //----------------------
+    html = "<div></div>"
+         + "<h3>Map: Cities with over 9 million population</h3>"
+         + '<h4>what level is the Population, please click!</h4>'
+         + '<p class="main">The population for this city is<span class="population"></span> x 10,000 people.</p>'
+    $('.explanation').empty();
+    $('.explanation').append(html);
+
+    //----------------------
+
+    map.removeLayer(layerMappedMarkers);
+    filteredData = _.filter(parsedData.features,function(datum){
+      return datum.properties.Population > 900;
+    });
+    console.log(filteredData);
+    filteredData = {
+      type: "FeatureCollection",
+      features: filteredData
+    };
+
+    //Add layer click event in this chained function calls
+    layerMappedMarkers = L.geoJSON(filteredData)
+                          .eachLayer(function(layer){
+                            layer.on('click', function(){
+                              console.log('clicked');
+                              console.log(layer.feature.properties.Population);
+                              $('.population').text(" " + layer.feature.properties.Population);
+                            });
+                          })
+                          .addTo(map);
+
+  }else if(slideNumber == 3){
+    console.log(slideNumber);
+    map.removeLayer(layerMappedMarkers);
+    filteredData = _.filter(parsedData.features,function(datum){
+      return parseFloat(datum.properties['investment proportion'])>50;
+    });
+    console.log(filteredData);
+    filteredData = {
+      type: "FeatureCollection",
+      features: filteredData
+    };
+    layerMappedMarkers = L.geoJSON(filteredData).addTo(map);
+
+    //----------------------
+    html = "<div></div>"
+         + '<h3>Map: Cities with higher than 50% investment in its GDP</h3>'
+         + '<h4>what level is the investment ratio, please click!</h4>'
+         + '<p class="main">The investment ratio for this city is<span class="investment"></span>.</p>'
+    $('.explanation').empty();
+    $('.explanation').append(html);
+
+    //----------------------
+
+  }else{
+    slideNumber = 0;
+    console.log(slideNumber);
+    $('#previous').hide();
+    map.removeLayer(layerMappedMarkers);
+    layerMappedMarkers = L.geoJson(parsedData)
+                          .eachLayer(function(layer){
+                            layer.on('click', function(){
+                              console.log('clicked');
+                              console.log(layer.feature.properties['investment proportion']);
+                              $('.investment').text(" " + layer.feature.properties['investment proportion']);
+                            });
+                          }).addTo(map);
+  }
+};
+
+/////////////////CLICK PREVIOUS BUTTON
+var clickPreviousButton = function(){
+  var filteredData;
+  var html;
+  slideNumber--;
+  if(slideNumber == 1){
+
+    html = "<div></div>"
+         + '<h3>Map: Cities with High Level GDP</h3>'
+         + '<h4>what level is the GDP, please click!</h4>'
+         + '<p class="main">The GDP for this city is<span class="GDP"></span>.</p>'
+
+    $('.explanation').empty();
+    $('.explanation').append(html);
+
+
+    console.log(slideNumber);
+    $('#previous').show();
+    map.removeLayer(layerMappedMarkers);
+    filteredData = _.filter(parsedData.features,function(datum){
+      return datum.properties.GDPlevel === "high";
+    });
+    filteredData = {
+      type: "FeatureCollection",
+      features: filteredData
+    };
+    layerMappedMarkers = L.geoJSON(filteredData)
+                          .eachLayer(function(layer){
+                            layer.on('click', function(){
+                              console.log('clicked');
+                              console.log(layer.feature.properties.GDP);
+                              $('.GDP').text(" " + layer.feature.properties.GDP);
+                            });
+                          })
+                          .addTo(map);
+
+
+  }else if(slideNumber == 2){
+
+        html = "<div></div>"
+             + "<h3>Map: Cities with over 9 million population</h3>"
+             + '<h4>what level is the Population, please click!</h4>'
+             + '<p class="main">The population for this city is<span class="population"></span> x 10,000 people.</p>'
+        $('.explanation').empty();
+        $('.explanation').append(html);
+
+
+    console.log(slideNumber);
+    $('#previous').show();
+    map.removeLayer(layerMappedMarkers);
+    filteredData = _.filter(parsedData.features,function(datum){
+      return datum.properties.Population > 900;
+    });
+
+    console.log(filteredData);
+    filteredData = {
+      type: "FeatureCollection",
+      features: filteredData
+    };
+    layerMappedMarkers = L.geoJSON(filteredData)
+                          .eachLayer(function(layer){
+                            layer.on('click', function(){
+                              console.log('clicked');
+                              console.log(layer.feature.properties.Population);
+                              $('.population').text(" " + layer.feature.properties.Population);
+                            });
+                          })
+                          .addTo(map);
+
+  }else{
+    console.log(slideNumber);
+    $('#previous').hide();
+    map.removeLayer(layerMappedMarkers);
+    layerMappedMarkers = L.geoJson(parsedData).addTo(map);
+  }
+};
+
+
+$(document).ready(function() {
+  $('#previous').hide();
+  $.ajax(dataset).done(function(data) {
+    parsedData = JSON.parse(data);
+    layerMappedMarkers = L.geoJson(parsedData).addTo(map);
+    console.log(parsedData);
   });
 
-var eachFeatureFunction = function(layer) {
-   layer.on('click', function (event) {
-   document.getElementById("results").style.display = "inline";
-   console.log(layer.feature);
-     switch (layer.feature.properties.GDPlevel){
-       case 'high':
-         $('.day-of-week').text('Monday');
-         break;
-       case 'medium':
-         $('.day-of-week').text('Tuesday');
-         break;
-       case 'low':
-         $('.day-of-week').text('Wednesday');
-         break;
-   }
- });
-};
+  $('#next').click(function(){
+    clickNextButton();
+  });
+
+  $('#previous').click(function(){
+    clickPreviousButton();
+  });
+});
